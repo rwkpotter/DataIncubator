656c1d4cc8d435c03731d40128edaaac93f20d64
rwkpotter <rwkpotter@gmail.com>
2016-09-14 14:15:39 -0400

    popularity caption

--- stationsbypopularity.php
+++ stationsbypopularity.php
@@ -8,7 +8,9 @@
 			      		<iframe width="100%" height="520" frameborder="0" src="https://rwkpotter.carto.com/viz/d473213c-7953-11e6-b63d-0ef24382571b/embed_map" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
 
 			        	<figcaption style="line-height:1.5; font-size: 16px; color:rgb(130,130,130);">
-					        <p style="margin-top:60px; float:none; clear:both; padding-top: 25px;">These graphs illustrate the number of trips started form each station for July 2013, June 2014, June 2015, and June 2016. The dark blues and purples highlight the most accessed starting stations. The most popular starting stations were in Manhattan. The 2016 graph includes the expansion of stations into other boroughs. These newer stations have, so far, received far less users than those in Manhattan. The most accessed stations each year were: 2013 - W 20 St &amp; 11 Ave (station 459: 40.74674 lat, -74.00776 long, in Chelsea); 2014 and 2015 - 8 Ave &amp; W 31 St (station 521: 40.75097 lat, -73.99444 long, next to Madison Square Gardens); and 2016 - Pershing Square North (station 519: 40.75187 lat, -73.97771 long, next to Grand Central Station). Pershing Square North, Lafayette St &amp; E 8 St (station 293: 40.73029 lat, -73.99076 long, next to Astor Place Metro), West St &amp; Chambers St (station 426: 40.71755 lat, -74.01322 long, near Tribeca Performing Arts Center) E 17 St &amp; Broadway (station 497: 40.73705 lat, -73.99009 long, next to Union Square Park) have been one of the 10 most accessed stations each year.</p>
+					        <p style="margin-top:60px; float:none; clear:both; padding-top: 25px;">This interactive map can be used to view the location of each bike station and their popularity, represented by the number of journeys started at each station. The datasets available are for four quarters: the first quarter of the scheme's operation (Q3-2013); and then Q2 for the years 2014, 2015, and 2016. Note that the scale changes for each dataset on the map. The most accessed stations for each of the quarters were: 2013 - W 20 St &amp; 11 Ave (station 459: 40.74674 lat, -74.00776 long;
+							2014 - E 42 St &amp; Vanderbilt Ave (station 519: 40.752416 lat, -73.97837); 2015 and 2016 - Pershing Square North (station 519: 40.75187 lat, -73.97771 long).</p>
+							<p>Quarters are defined as: Q1 - January to March; Q2 - April to June; Q3 - July to September; Q4 - October to December.</p>
 			        	</figcaption>
 			      	</figure>
 			    </section>
@@ -31,4 +33,9 @@
 
 
 https://rwkpotter.carto.com/api/v2/viz/d473213c-7953-11e6-b63d-0ef24382571b/viz.json
--->
+
+
+	<figcaption style="line-height:1.5; font-size: 16px; color:rgb(130,130,130);">
+					        <p style="margin-top:60px; float:none; clear:both; padding-top: 25px;">These graphs illustrate the number of trips started form each station for July 2013, June 2014, June 2015, and June 2016. The dark blues and purples highlight the most accessed starting stations. The most popular starting stations were in Manhattan. The 2016 graph includes the expansion of stations into other boroughs. These newer stations have, so far, received far less users than those in Manhattan. The most accessed stations each year were: 2013 - W 20 St &amp; 11 Ave (station 459: 40.74674 lat, -74.00776 long, in Chelsea); 2014 and 2015 - 8 Ave &amp; W 31 St (station 521: 40.75097 lat, -73.99444 long, next to Madison Square Gardens); and 2016 - Pershing Square North (station 519: 40.75187 lat, -73.97771 long, next to Grand Central Station). Pershing Square North, Lafayette St &amp; E 8 St (station 293: 40.73029 lat, -73.99076 long, next to Astor Place Metro), West St &amp; Chambers St (station 426: 40.71755 lat, -74.01322 long, near Tribeca Performing Arts Center) E 17 St &amp; Broadway (station 497: 40.73705 lat, -73.99009 long, next to Union Square Park) have been one of the 10 most accessed stations each year.</p>
+			        	</figcaption>
+			        	-->
