13291c590b3e6b2d858b48c3dedaaeddc8e5b798
rwkpotter <rwkpotter@gmail.com>
2016-09-14 10:52:31 -0400

    add jrnys per qtr

--- usersbygender.php
+++ usersbygender.php
@@ -3,8 +3,8 @@
   			<div id="container" class="clear">
   				 <section class="clear">
 			      	<figure>
-			      		<h2 style="text-align:center; margin-bottom:15px; font-style:none; color:rgb(51,51,51); font-family: sans-serif;">Data analysis: Users by Gender</h2>
-			      		<img src="img/journeysperqtr.png" alt="" style="float:left; margin-right:50px;">
+			      		<h2 style="text-align:center; margin-bottom:15px; font-style:none; color:rgb(51,51,51); font-family: sans-serif;">Data analysis: Users b Gender</h2>
+			      		<img src="img/UsersbyGender.png" alt="" style="float:left; margin-right:50px;">
 			        	<figcaption style="line-height:1.5; font-size: 16px; color:rgb(130,130,130);">
 			        		<p style="margin-top:60px;">This graph illustrates the gender distribution for the users of the New York City Citi Bike program between January 2015 (in red) and January 2016 (in blue). Note that 'unknown' refers to people using the program as customers (rather than full annual subscribers), who are not required to provide their gender. The graph demonstrates an increase in users, of all genders, in the 12 months from January 2015 to January 2016. </p>
 							<p>These data demonstrate that the majority of users are male. This suggests that Citi bike marketing may wish to focus advertising and incentives on women to join the scheme, to maximize the program's growth and revenue.</p>
