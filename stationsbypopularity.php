2f5cc3ffe2152c010eade1ee432d06bd0758b35e
rwkpotter <rwkpotter@gmail.com>
2016-09-14 08:19:17 -0400

    map test

--- stationsbypopularity.php
+++ stationsbypopularity.php
@@ -5,10 +5,8 @@
   				 <section class="clear">
 			      	<figure>
 			      		<h2 style="text-align:center; margin-bottom:15px; font-style:none; color:rgb(51,51,51); font-family: sans-serif;">Data Analysis: Starting Stations by Popularity</h2>
-			      		<img src="img/startbypop201307.png" alt="" style="width:45%; float:left; margin-right:5%;">
-			      		<img src="img/startbypop201406.png" alt="" style="width:45%; float:left; margin-right:0;">
-			      		<img src="img/startbypop201506.png" alt="" style="width:45%; float:left; margin-right:5%;">
-			      		<img src="img/startbypop201606.png" alt="" style="width:45%; float:left; margin-right:0;">
+			      		<iframe width="100%" height="520" frameborder="0" src="https://rwkpotter.carto.com/viz/d473213c-7953-11e6-b63d-0ef24382571b/embed_map" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+
 			        	<figcaption style="line-height:1.5; font-size: 16px; color:rgb(130,130,130);">
 					        <p style="margin-top:60px; float:none; clear:both; padding-top: 25px;">These graphs illustrate the number of trips started form each station for July 2013, June 2014, June 2015, and June 2016. The dark blues and purples highlight the most accessed starting stations. The most popular starting stations were in Manhattan. The 2016 graph includes the expansion of stations into other boroughs. These newer stations have, so far, received far less users than those in Manhattan. The most accessed stations each year were: 2013 - W 20 St &amp; 11 Ave (station 459: 40.74674 lat, -74.00776 long, in Chelsea); 2014 and 2015 - 8 Ave &amp; W 31 St (station 521: 40.75097 lat, -73.99444 long, next to Madison Square Gardens); and 2016 - Pershing Square North (station 519: 40.75187 lat, -73.97771 long, next to Grand Central Station). Pershing Square North, Lafayette St &amp; E 8 St (station 293: 40.73029 lat, -73.99076 long, next to Astor Place Metro), West St &amp; Chambers St (station 426: 40.71755 lat, -74.01322 long, near Tribeca Performing Arts Center) E 17 St &amp; Broadway (station 497: 40.73705 lat, -73.99009 long, next to Union Square Park) have been one of the 10 most accessed stations each year.</p>
 			        	</figcaption>
@@ -24,3 +22,13 @@
 		</div>
 	</body>
 </html>
+
+<!--
+<img src="img/startbypop201307.png" alt="" style="width:45%; float:left; margin-right:5%;">
+			      		<img src="img/startbypop201406.png" alt="" style="width:45%; float:left; margin-right:0;">
+			      		<img src="img/startbypop201506.png" alt="" style="width:45%; float:left; margin-right:5%;">
+			      		<img src="img/startbypop201606.png" alt="" style="width:45%; float:left; margin-right:0;">
+
+
+https://rwkpotter.carto.com/api/v2/viz/d473213c-7953-11e6-b63d-0ef24382571b/viz.json
+-->
